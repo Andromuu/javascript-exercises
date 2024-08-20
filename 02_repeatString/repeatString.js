394bb5009cb0b9b69026761a2ca3273e445f5a62
Andromu <andresrodriguez3d@gmail.com>
2024-08-20 10:53:56 +0100

    Solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+const repeatString = function( string, num) {
+    let phrase = "";
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            phrase = `${phrase}${string}`;
+        }
+    } else {
+        phrase = "ERROR";
+    }
+    return phrase;
 };
 
 // Do not edit below this line
